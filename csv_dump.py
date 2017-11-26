df6080a883409757e0d70b53ee3cf38a97deaadb
Bowen Wang <bowenwang@Bowens-MacBook-Pro.local>
2017-11-26 00:29:28 -0500

    bug fixed for image info extraction and final csv dumped

--- csv_dump.py
+++ csv_dump.py
@@ -39,7 +39,13 @@ def readImgParameters(filename):
 
     for x in range(0, xs):
         for y in range(0, ys):
-            [r, g, b] = img[x, y]
+            if type(img[x, y]) == type(1):
+                r = g = b = img[x, y]
+            else:
+                if len(img[x,y]) == 4:
+                    [r, g, b, alpha] = img[x, y]
+                else:
+                    [r, g, b] = img[x, y]
             rs.append(r/255.0)
             gs.append(g/255.0)
             bs.append(b/255.0)
@@ -67,6 +73,7 @@ poster_list = listdir(poster_path)
 del poster_list[0]
 
 
+
 with open(genres_path) as json_data:
 	obj = json.load(json_data)
 obj = obj['genres']
@@ -75,7 +82,7 @@ genres_list = []
 for genre in obj:
 	genres_list.append(genre['id'])
 
-print "All genres id:",genres_list
+# print "All genres id:",genres_list
 
 with open(movie_path) as json_data:
     obj = json.load(json_data)
@@ -91,24 +98,23 @@ for i in range(len(obj)):
 			continue
 		movie_list.append(obj[i])
 
-print len(movie_list)
+# print len(movie_list)
 
 writer = csv.writer(open(result_path,'wt'))
-column_name = ['id','title','adult','budget','date','duration','r','g','b','mainHue','avgHue','avgSat','avgLtn','actor-1','actor-2','actor-3','director']
+column_name = ['id','adult','budget','date','duration','r','g','b','mainHue','avgHue','avgSat','avgLtn','actor-1','actor-2','actor-3','director']
 genres_array = []
 for i in range(len(genres_list)):
 	genres_array.append('genre-'+str(i+1))
 column_name = column_name + genres_array
 column_name.append('rating')
-print len(column_name)
+# print len(column_name)
 writer.writerow(column_name)
 
 start_time = time.time()
-for i in range(40):
+for i in range(len(movie_list)):
 	column = [movie_list[i]['id'],
-				movie_list[i]['title'],
-				movie_list[i]['budget'],
 				movie_list[i]['adult'],
+				movie_list[i]['budget'],
 				movie_list[i]['date'],
 				movie_list[i]['duration']]
 	img_params = readImgParameters(poster_path + poster_list[i])
@@ -137,17 +143,13 @@ for i in range(40):
 	writer.writerow(column)
 
 	end_time = time.time()
+	now_time = end_time - start_time
+	remaining_time = (1.0 * (end_time - start_time)) / (1.0*(i+1)/len(movie_list)) - now_time
+	remaining_min = int(remaining_time / 60)
+	remaining_sec = int(remaining_time) % 60
 
-	print "[",i+1,"/",len(movie_list),"] Being written.......","{0:.2f}".format(end_time - start_time),"s.........",'{:.2%}'.format(1.0*(i+1)/len(movie_list))
+	print "[",i+1,"/",len(movie_list),"] Being written.......","{0:.2f}".format(now_time),"s/",remaining_min,"min",remaining_sec,"s.........",'{:.2%}'.format(1.0*(i+1)/len(movie_list)),"[ID]:",movie_list[i]['id']
 print "write finished"
-
-
-
-
-
-
-
-
 
 
 
